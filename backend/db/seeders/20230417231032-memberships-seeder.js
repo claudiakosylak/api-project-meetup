4e4ef80ab335119386db2398fcdd3b1605c74608
Your Name <claudiakosylak@gmail.com>
2023-04-17 18:13:36 -0500

    added memberships seeder

---
+++ backend/db/seeders/20230417231032-memberships-seeder.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    options.tableName = 'Memberships';
+    await queryInterface.bulkInsert(options, [
+      {
+        userId: 1,
+        groupId: 2,
+        status: "co-host"
+      },
+      {
+        userId: 2,
+        groupId: 3,
+        status: "member"
+      },
+      {
+        userId: 3,
+        groupId: 1,
+        status: "pending"
+      }
+    ], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Memberships';
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(options, {
+      id: { [Op.in]: [1, 2, 3] }
+    }, {});
+  }
+};
