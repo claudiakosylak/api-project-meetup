1b0726befdb02d0573f1c4a659c424a474ccef42
Your Name <claudiakosylak@gmail.com>
2023-04-20 16:45:10 -0500

    successful feature get all events

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,8 +1,46 @@
 const express = require('express');
+const { Event, Group, Venue, Attendance, EventImage } = require('../../db/models');
 
 const { requireAuth } = require('../../utils/auth');
 
 const router = express.Router();
+
+router.get("/", async (req, res) => {
+    const events = await Event.findAll({
+        attributes: ["id", "groupId", "venueId", "name", "type", "startDate", "endDate"],
+        include: [{
+            model: Group,
+            attributes: ["id", "name", "city", "state"]
+        },
+    {
+        model: Venue,
+        attributes: ["id", "city", "state"]
+    }]
+    })
+
+    console.log("events:", events);
+
+    for (let event of events) {
+        const attendances = await Attendance.count({
+            where: {
+                eventId: event.id
+            }
+        })
+
+        event.dataValues.numAttending = attendances;
+
+        const prevImage = await EventImage.findOne({
+            where: {
+                eventId: event.id,
+                preview: true
+            }
+        })
+
+        event.dataValues.previewImage = prevImage.url;
+    }
+
+    return res.json(events);
+})
 
 
 router.use((err, req, res, next) => {
