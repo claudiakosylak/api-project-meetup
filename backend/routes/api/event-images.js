f7e7ef16611006255d181e99f79825be185d1cdd
Your Name <claudiakosylak@gmail.com>
2023-04-21 10:10:45 -0500

    believe have delete image done but now need to still fitst create it to test right

--- backend/routes/api/event-images.js
+++ backend/routes/api/event-images.js
@@ -1,11 +1,55 @@
 const express = require('express');
-const { Event, Group, Venue, Attendance, EventImage } = require('../../db/models');
+const { Event, Group, EventImage, Membership } = require('../../db/models');
 
 const { requireAuth } = require('../../utils/auth');
 
 const router = express.Router();
 
+router.delete("/:imageId", requireAuth, async (req, res) => {
+    const { imageId } = req.params;
+    const image = await EventImage.findOne({
+        where: {
+            id: imageId
+        },
+        include: {
+            model: Event
+        }
+    })
 
+    if (!image) {
+        res.status(404);
+        return res.json({"message": "Event image couldn't be found"})
+    }
+
+
+
+    const group = await Group.findOne({
+        where: {
+            id: image.Event.groupId
+        }
+    })
+
+    const memberships = await Membership.findAll({
+        where: {
+            groupId: group.id,
+            userId: req.user.id,
+            status: "co-host"
+        }
+    })
+
+    console.log(memberships)
+
+    if (group.organizerId !== req.user.id && memberships.length === 0) {
+        res.status(403);
+        return res.json({"message": "Forbidden"})
+    }
+
+    image.destroy()
+
+    return res.json({"message": "Event Image couldn't be found"})
+
+
+})
 
 
 
