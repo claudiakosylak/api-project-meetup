0c2a5a8bba3bbe750d02f04cd7a874e4b47673fe
Your Name <claudiakosylak@gmail.com>
2023-04-21 14:47:40 -0500

    success on delete group image

--- backend/routes/api/group-images.js
+++ backend/routes/api/group-images.js
@@ -4,6 +4,39 @@ const { requireAuth } = require('../../utils/auth');
 
 const router = express.Router();
 
+router.delete("/:imageId", requireAuth, async (req, res) => {
+    const { imageId } = req.params;
+    const image = await GroupImage.findOne({
+        where: {
+            id: imageId
+        },
+        include: {
+            model: Group,
+            attributes: ["id", "organizerId"]
+        }
+    })
+
+    if (!image) {
+        res.status(404);
+        return res.json({"message": "Group image couldn't be found"})
+    }
+
+    const memberships = await Membership.findAll({
+        where: {
+            groupId: image.Group.id,
+            userId: req.user.id,
+            status: "co-host"
+        }
+    })
+
+    if (image.Group.organizerId !== req.user.id && memberships.length === 0) {
+        res.status(403);
+        return res.json({"message": "Forbidden"})
+    }
+
+    image.destroy()
+    return res.json({"message": "Successfully deleted"})
+})
 
 
 router.use((err, req, res, next) => {
