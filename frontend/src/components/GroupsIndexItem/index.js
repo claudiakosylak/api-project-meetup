58ef63f49ba717137938f31953e2c047e5d8323a
Your Name <claudiakosylak@gmail.com>
2023-05-08 11:28:03 -0500

    light formatting for now on all groups page css

--- frontend/src/components/GroupsIndexItem/index.js
+++ frontend/src/components/GroupsIndexItem/index.js
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom/cjs/react-router-dom.min"
+import "./GroupsIndexItem.css";
 
 
 
 const GroupsIndexItem = ({group}) => {
-    console.log("GROUP INSIDE GROUPINDEXITEM COMPONENT: ", group)
 
     return (
         <li className="groups-index-item-container">
-            <Link to={`/groups/${group.id}`}>{group.name}</Link>
+            <div className="group-image-placeholder"></div>
+            <div className="group-index-text-container">
+            <h2><Link to={`/groups/${group.id}`}>{group.name}</Link></h2>
+            <p>{group.city}, {group.state}</p>
+            <p>{group.about}</p>
+            <div className="under-groups-text-container">
+                <p>## Events</p>
+                <p>{group.private ? "Private" : "Public"}</p>
+            </div>
+            </div>
         </li>
     )
 }
