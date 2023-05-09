8a701c13fbb667e5d6165167f60926995b6fcb73
Your Name <claudiakosylak@gmail.com>
2023-05-09 11:24:52 -0500

    connected update group component

--- frontend/src/components/UpdateGroupForm/index.js
+++ frontend/src/components/UpdateGroupForm/index.js
@@ -3,21 +3,26 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 
 import GroupForm from "../GroupForm";
+import { getGroupThunk } from "../../store/groups";
 
 const UpdateGroupForm = () => {
     const dispatch = useDispatch();
     const {groupId} = useParams();
     const group = useSelector(state => state.groups[groupId]);
 
+    useEffect(() => {
+        dispatch(getGroupThunk(groupId))
+    }, [dispatch, groupId])
+
     if (!group) return(null);
 
     return (
-        Object.keys(group).length > 1 && (
+
             <GroupForm
                 group={group}
                 formType="Update Group"
             />
-        )
+
     )
 }
 
