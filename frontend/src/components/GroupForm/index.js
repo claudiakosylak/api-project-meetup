e22e128defdd12aaa51b23300278594a2ef29f0a
Your Name <claudiakosylak@gmail.com>
2023-05-10 11:44:15 -0500

    formatted the form for both create and update group and made conditionals for update or create

--- frontend/src/components/GroupForm/index.js
+++ frontend/src/components/GroupForm/index.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import {useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { createGroupThunk, updateGroupThunk } from "../../store/groups";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import "./GroupForm.css";
 
-const GroupForm = ({group, formType}) => {
+const GroupForm = ({ group, formType }) => {
     const dispatch = useDispatch()
     const types = ["In Person", "Online"];
     const history = useHistory();
@@ -14,8 +15,8 @@ const GroupForm = ({group, formType}) => {
 
     const [name, setName] = useState(group?.name);
     const [about, setAbout] = useState(group?.about);
-    const [type, setType] = useState(group.type ? group.type : "In Person");
-    const [privateStatus, setPrivateStatus] = useState(group.private ? group.private : false);
+    const [type, setType] = useState(group.type ? group.type : "");
+    const [privateStatus, setPrivateStatus] = useState(group.private ? group.private : "");
     const [location, setLocation] = useState(groupLocation ? groupLocation : "");
     const [imageUrl, setImageUrl] = useState("");
     const [errors, setErrors] = useState({});
@@ -84,83 +85,133 @@ const GroupForm = ({group, formType}) => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div className="form-section">
-                <h2>BECOME AN ORGANIZER</h2>
-                <p className="form-headers">We'll walk you through a few steps to build your local community</p>
-            </div>
-            <div className="form-section">
-                <p className="form-headers">First, set your group's location.</p>
-                <p className="form-text">ReadUp groups meet locally, in person and online. We'll connect you with readers in your area, and more can join you online.</p>
-                <input className="location-input form-inputs"
-                    type="text"
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}
-                    placeholder="city, STATE"
-                />
-                {(hasSubmitted && errors.location) && (
-                    <p className="errors">{errors.location}</p>
+        <div className="group-form-container">
+
+            <form onSubmit={handleSubmit} className="group-form">
+                {formType === "Create Group" ? (
+                    <div className="form-section">
+                        <h2>BECOME AN ORGANIZER</h2>
+                        <p className="form-headers">We'll walk you through a few steps to build your local community</p>
+
+                    </div>
+                ) : (
+                    <div className="form-section">
+                        <h2>UPDATE YOUR GROUP'S INFORMATION</h2>
+                        <p className="form-headers">We'll walk you through a few steps to update your group's information</p>
+
+                    </div>
                 )}
-            </div>
-            <div className="form-section">
-                <p className="form-headers">What will your group's name be?</p>
-                <p className="form-text">Choose a name that will give people a clear idea of what the group will be reading.
-                <br />Feel free to get creative! You can edit this later if you change your mind.
-                </p>
-                <input className="form-inputs"
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    placeholder="What is your reading group name?"
-                />
-            </div>
-            <div className="form-section">
-                <p className="form-headers">Now describe what your group will be reading</p>
-                <p className="form-text">People will see this when we promote your group, but you'll be able to add it later, too.
-                <br /><br />1. What's the purpose of the group?
-                <br />2. Who should join?
-                <br />3. What will you do at your events?
-                </p>
-                <textarea className="form-inputs"
-                    value={about}
-                    onChange={(e) => setAbout(e.target.value)}
-                    placeholder="Please write at least 30 characters"
-                />
-            </div>
-            <div className="form-section">
-                <p className="form-headers">Final steps...</p>
-                <p className="form-text">Is this an in person or online group?</p>
-                <select className="form-dropdowns"
-                    value={type}
-                    onChange={(e) => setType(e.target.value)}
-                >
-                    {types.map(type => (
-                        <option
-                            key={type}
-                            value={type}
-                        >
-                            {type}
-                        </option>
-                    ))}
-                </select>
-                <p className="form-text">Is this group private or public?</p>
-                <select className="form-dropdowns"
-                    value={privateStatus}
-                    onChange={(e) => setPrivateStatus(e.target.value)}
-                >
-                    <option value={false}>Public</option>
-                    <option value={true}>Private</option>
-                </select>
-                <p className="form-text">Please add an image url for your group below:</p>
-                <input className="form-inputs"
-                    type="text"
-                        value={imageUrl}
-                        onChange={(e) => setImageUrl(e.target.value)}
-                        placeholder="Image Url"
-                />
-            </div>
-            <button type="submit">Create group</button>
-        </form>
+                <div className="form-section">
+                    <p className="form-headers">First, set your group's location.</p>
+                    <p className="form-text">ReadUp groups meet locally, in person and online. We'll connect you with readers in your area, and more can join you online.</p>
+                    <input className="location-input form-inputs"
+                        type="text"
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                        placeholder="city, STATE"
+                    />
+                    {(hasSubmitted && errors.location) && (
+                        <p className="errors">{errors.location}</p>
+                    )}
+                </div>
+                {formType === "Create Group" ? (
+                    <div className="form-section">
+                        <p className="form-headers">What will your group's name be?</p>
+                        <p className="form-text">Choose a name that will give people a clear idea of what the group will be reading.
+                            <br />Feel free to get creative! You can edit this later if you change your mind.
+                        </p>
+                        <input className="form-inputs"
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="What is your reading group name?"
+                        />
+                    </div>
+
+                ) : (
+                    <div className="form-section">
+                        <p className="form-headers">What is the name of your group?</p>
+                        <p className="form-text">Choose a name that will give people a clear idea of what the group will be reading.
+                            <br />Feel free to get creative!
+                        </p>
+                        <input className="form-inputs"
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="What is your reading group name?"
+                        />
+                    </div>
+                )}
+
+                <div className="form-section">
+                    <p className="form-headers">Now describe what your group will be reading</p>
+                    {formType === "Create Group" ? (
+                        <p className="form-text">People will see this when we promote your group, but you'll be able to add it later, too.
+                            <br /><br />1. What's the purpose of the group?
+                            <br />2. Who should join?
+                            <br />3. What will you do at your events?
+                        </p>
+
+                    ) : (
+                        <p className="form-text">People will see this when we promote your group.
+                            <br /><br />1. What's the purpose of the group?
+                            <br />2. Who should join?
+                            <br />3. What will you do at your events?
+                        </p>
+                    )}
+                    <textarea className="form-inputs form-description"
+                        value={about}
+                        onChange={(e) => setAbout(e.target.value)}
+                        placeholder="Please write at least 30 characters"
+                    />
+                </div>
+                <div className="form-section">
+                    <p className="form-headers">Final steps...</p>
+                    <p className="form-text">Is this an in person or online group?</p>
+                    <select className="form-dropdowns"
+                        value={type}
+                        onChange={(e) => setType(e.target.value)}
+                    >
+                        <option value="" disabled>(select one)</option>
+                        {types.map(type => (
+                            <option
+                                key={type}
+                                value={type}
+                            >
+                                {type}
+                            </option>
+                        ))}
+                    </select>
+                    <p className="form-text">Is this group private or public?</p>
+                    <select className="form-dropdowns"
+                        value={privateStatus}
+                        onChange={(e) => setPrivateStatus(e.target.value)}
+                    >
+                        <option value="" disabled>(select one)</option>
+                        <option value={false}>Public</option>
+                        <option value={true}>Private</option>
+                    </select>
+                    {formType === "Create Group" && (
+                        <>
+                            <p className="form-text">Please add an image url for your group below:</p>
+                            <input className="form-inputs"
+                                type="text"
+                                value={imageUrl}
+                                onChange={(e) => setImageUrl(e.target.value)}
+                                placeholder="Image Url"
+                            />
+
+                        </>
+                    )}
+                </div>
+                {formType === "Create Group" ? (
+
+                    <button type="submit" className="group-form-submit-button">Create group</button>
+                ): (
+                    <button type="submit" className="group-form-submit-button">Update group</button>
+                )}
+            </form>
+        </div>
     )
 }
 
