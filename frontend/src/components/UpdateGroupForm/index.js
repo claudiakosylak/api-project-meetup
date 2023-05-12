4c8f405bb8bb20f0969dd75aed07be7e619a1136
Your Name <claudiakosylak@gmail.com>
2023-05-12 17:10:08 -0500

    added reroute to landing page if not logged in or not authorized and try to update a group

--- frontend/src/components/UpdateGroupForm/index.js
+++ frontend/src/components/UpdateGroupForm/index.js
@@ -4,17 +4,28 @@ import { useEffect } from "react";
 
 import GroupForm from "../GroupForm";
 import { getGroupThunk } from "../../store/groups";
+import { useHistory } from "react-router-dom";
 
 const UpdateGroupForm = () => {
+    const history = useHistory();
     const dispatch = useDispatch();
     const {groupId} = useParams();
     const group = useSelector(state => state.groups.currentGroup);
+    const sessionUser = useSelector(state => state.session.user);
 
     useEffect(() => {
         dispatch(getGroupThunk(groupId))
     }, [dispatch, groupId])
 
     if (!group) return(null);
+    if (!sessionUser) {
+        history.push("/");
+        return null;
+    }
+    if (!sessionUser || sessionUser.id !== group.organizerId){
+     history.push("/");
+     return null;
+    }
 
     console.log("UPDATE GROUP PRELOAD: ", group)
 
